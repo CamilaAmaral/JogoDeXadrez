770fa766a263c11a61d51a28dc9bf4c55e3c8ec9
CamilaAmaral <camila_dma@hotmail.com>
2020-11-19 12:17:53 -0300

    Criada classe de peças, tabuleiros e feita a construção da impressão do tabuleiro.

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import xadrez.PartidaXadrez;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		PartidaXadrez partidaXadrez = new PartidaXadrez();
+		UI.printTabuleiro(partidaXadrez.getPecas());
+	}
+
+}
---
+++ src/xadrez/PartidaXadrez.java
@@ -0,0 +1,25 @@
+package xadrez;
+
+import tabuleiroJogo.Tabuleiro;
+
+public class PartidaXadrez {
+
+	private Tabuleiro tabuleiro;
+
+	public PartidaXadrez() {
+		tabuleiro = new Tabuleiro(8, 8);
+	}
+
+	// Retorna uma matriz de peças de xadrez correspondentes a partida;
+	public PecaXadrez[][] getPecas() {
+		PecaXadrez[][] matriz = new PecaXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];
+		for (int i = 0; i < tabuleiro.getLinhas(); i++) {
+			for (int j = 0; j < tabuleiro.getColunas(); j++) {
+				matriz[i][j] = (PecaXadrez) tabuleiro.peca(i, j);
+
+			}
+		}
+		return matriz;
+	}
+
+}
